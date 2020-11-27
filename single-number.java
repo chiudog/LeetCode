4c166158d4e005ea8cda21745667f1457ad38de3
Brian Chiu <75084193+codeclimbs@users.noreply.github.com>
2020-11-26 22:20:16 -0600

    Update single-number.java

--- single-number.java
+++ single-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
+     
         for(int n : nums){
             result ^= n;
         }
+     
         return result;
     }
 }
